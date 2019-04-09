cbc761c3c1534f757eaa0ebf34c10964da08296c
Hoang Cao <hoangcao@mail.weber.edu>
2019-04-09 13:31:23 -0700

    finish function at()

--- digital-time.js
+++ digital-time.js
@@ -4,14 +4,30 @@ exports.at = at;
 // Rewrite this file as needed.
 //
 let date = require('date-and-time');
-let now = new Date();
- 
 
 function at(hours, minutes) {
+
+  let a = hours;
+  let b = minutes;
   
-  let now = new Date();
-  return date.format(now, 'YYYY/MM/DD HH:mm:ss');    // => '2015/01/02 23:14:05'
-  date.format(now, 'ddd MMM DD YYYY');        // => 'Fri Jan 02 2015'
-  date.format(now, 'hh:mm A [GMT]Z');         // => '11:14 p.m. GMT-0800'
-  date.format(now, 'hh:mm A [GMT]Z', true); 
+  let now;
+ 
+  if(typeof a !== "undefined" && typeof b !== "undefined")
+  {
+    now = new Date(2019,9,4,a,b);
+  }
+  else if(typeof a !== "undefined")
+  {
+    now = new Date(2019,9,4,a,0);
+  }
+  else if(typeof b !== "undefined")
+  {
+    now = new Date(2019,9,4,0,b);
+  }
+  return date.format(now, 'HH:mm');
 };
+
+function plus()
+{
+  
+}
