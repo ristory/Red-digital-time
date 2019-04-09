4f3ae83fe80888ca72bd3b6b77237bb1ed1a358f
Hoang Cao <hoangcao@mail.weber.edu>
2019-04-09 00:54:30 -0700

    add npm date-time

--- digital-time.js
+++ digital-time.js
@@ -3,8 +3,15 @@ exports.at = at;
 //
 // Rewrite this file as needed.
 //
+let date = require('date-and-time');
+let now = new Date();
+ 
 
 function at(hours, minutes) {
-
-  return '06:00'; // Just enough to make the 1st test pass.
+  
+  let now = new Date();
+  return date.format(now, 'YYYY/MM/DD HH:mm:ss');    // => '2015/01/02 23:14:05'
+  date.format(now, 'ddd MMM DD YYYY');        // => 'Fri Jan 02 2015'
+  date.format(now, 'hh:mm A [GMT]Z');         // => '11:14 p.m. GMT-0800'
+  date.format(now, 'hh:mm A [GMT]Z', true); 
 };
